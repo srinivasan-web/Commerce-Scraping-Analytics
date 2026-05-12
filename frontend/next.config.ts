44fab191812e19513e611671618631feaa10ba07
BaraneetharanSM <smbaraneetharan@gmail.com>
2026-05-12 23:04:46 +0530

    env-first

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   compress: true,
-  swcMinify: true,
   productionBrowserSourceMaps: false,
   httpAgentOptions: {
     keepAlive: true,
